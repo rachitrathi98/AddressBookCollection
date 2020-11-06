a623550cf39597f036fcbbe6180b2caf75afeba9
Rachit Rathi <rachitrathi98@gmail.com>
2020-11-06 23:53:47 +0530

    [Rachit] Add. Retrieve AddressBook Data

---
+++ AddressBook/AddressBookRepo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace AddressBook
+{
+    public class AddressBookRepoDB
+    {
+        public static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog =AddressBookService; User ID = racrathi; Password=Rachit123*";
+
+        static SqlConnection connection;
+
+        /// <summary>
+        /// UC16 RetrieveDataFromDB
+        /// </summary>
+        /// <returns></returns>
+        public static ContactsDB RetrieveData()
+        {
+            try
+            {
+                connection = new SqlConnection(connectionString);
+
+                ContactsDB contactsDB = new ContactsDB();
+
+                string query = "select c.FirstName, c.LastName, c.City, c.PhoneNumber, bk.B_Name, bk.B_Type from Contacts c inner join BookNameType bk on c.B_ID=bk.B_ID where c.FirstName='Rachit'";
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        contactsDB.first_name = reader.GetString(0);
+                        contactsDB.last_name = reader.GetString(1);
+                        contactsDB.city = reader.GetString(2);
+                        contactsDB.phone = reader.GetString(3);
+                        contactsDB.B_Name = reader.GetString(4);
+                        contactsDB.B_Type = reader.GetString(5);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found");
+                }
+                reader.Close();
+
+                return contactsDB;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+        }
+    }
+}
---
+++ AddressBookTest/UnitTest1.cs
@@ -0,0 +1,38 @@
+using AddressBook;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddressBookTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        /// <summary>
+        /// UC16
+        /// </summary>
+        [TestMethod]
+        public void CompareRetrievedDataFromDB()
+        {
+            ContactsDB expected = new ContactsDB()
+            {
+                first_name = "Rachit",
+                last_name = "Rathi",
+                city = "Juhu",
+                phone = "123456789",
+                B_Name = "Book1",
+                B_Type = "Family"
+            };
+            
+            var actual = AddressBookRepoDB.RetrieveData();
+
+            Assert.AreEqual(expected.first_name, actual.first_name);
+            Assert.AreEqual(expected.last_name, actual.last_name);
+            Assert.AreEqual(expected.city, actual.city);
+            Assert.AreEqual(expected.phone, actual.phone);
+            Assert.AreEqual(expected.B_Name, actual.B_Name);
+            Assert.AreEqual(expected.B_Type, actual.B_Type);
+
+        }
+
+    }
+}
